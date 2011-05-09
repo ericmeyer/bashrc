30932e28fee57f094d295bb4d279f8e3e2d116d2
Eric Meyer <emeyer@8thlight.com>
2011-05-09 07:42:12 -0500

    work on install

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
 #! /usr/bin/bash
 
-BASH_DIR=`pwd`
+. ./functions
 
+
+BASH_DIR=`pwd`
+if [[ -f $HOME/.gitconfig ]]; then
+  printf ".gitconfig exists.  Overwrite? (Y/n): "
+  choice
+  case "$CHOICE" in
+    [y] )
+      echo "  linking .gitconfig"
+      rm $HOME/.gitconfig
+      ln -s $BASH_DIR/config_files/.gitconfig $HOME/.gitconfig;;
+    [n] )
+      echo "  Skipping .gitconfig";;
+  esac
+else
+  ln -s $BASH_DIR/config_files/.gitconfig $HOME/.gitconfig
+fi
+
+echo ''
+echo ''
 echo '***************************************************'
 echo 'Add the following to your bash config file:'
 echo ''
