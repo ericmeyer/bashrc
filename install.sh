413e0c95f2504a31abe9aed5972cd04f8b98e34f
Eric Meyer <emeyer@8thlight.com>
2011-05-09 07:25:38 -0500

    Attempting to make this easier to use for other people

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+BASH_DIR=`pwd`
+
+echo '***************************************************'
+echo 'Add the following to your bash config file:'
+echo ''
+echo ". $BASH_DIR/source_all"
+echo '***************************************************'
